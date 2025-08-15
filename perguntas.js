df6adb3a17f6ca0fb9d433887541c95a3c5c9ad4
pedro005857 <pedro.santos.silva05@escola.pr.gov.br>
2025-08-15 15:01:29 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,35 +1,96 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Biologia',
+    'O que é o DNA?',
+    'O DNA (ácido desoxirribonucleico) é a molécula que carrega a informação genética '
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Biologia',
+    'Qual é o nome do órgão responsável por bombear o sangue no corpo humano?',
+    'Coração.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Biologia',
+    'O que é a seleção natural?',
+    'Características vantajosas aumentam sobrevivência'
 )
 
 criaCartao(
-    'Lingua inglesa',
+    'Biologia',
+    'Quais as diferenças entre células procarióticas e eucarióticas?',
+    'Procarióticas sem núcleo; eucarióticas com núcleo.'
+)
+
+criaCartao(
+    'Biologia',
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Biologia',
+    ' Como a perda de biodiversidade afeta os ecossistemas e a saúde humana?',
+    'Prejudica ecossistemas e saúde humana.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Biologia',
+    'Qual é a principal função do sistema nervoso?',
+    'Controlar e coordenar as funções do corpo, além de processar informações sensoriais e responder a estímulos.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é a célula responsável pela transmissão de impulsos nervosos no sistema nervoso?',
+    'O neurônio.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual parte do cérebro é responsável pelo equilíbrio e coordenação motora?',
+    'O cerebelo.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é a unidade básica da vida?',
+    'A Célula'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é o processo pelo qual as plantas produzem seu próprio alimento, utilizando luz solar?',
+    'Fotossíntese'
+)
+
+
+criaCartao(
+    'Biologia',
+    'Como são chamados os animais que se alimentam apenas de plantas?',
+    'Herbívoros.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual gás é liberado pelas plantas durante a fotossíntese?',
+    'Oxigênio.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é o processo de transformação de lagarta em borboleta?',
+    'Metamorfose.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é o pigmento responsável pela cor verde das plantas?',
+    'Clorofila.'
+) 
+
+criaCartao(
+    'Biologia',
+    'Como são chamadas as estruturas que transportam água nas plantas?',
+    'Xilema.'
 )
